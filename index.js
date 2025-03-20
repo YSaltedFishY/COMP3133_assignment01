bbfcd9d25976639a44803a6463140c09c287cfbe
YSaltedFishY <yxwyyhy@hotmail.com>
2025-03-19 23:29:25 -0400

    added cors

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const express = require('express');
 const mongoose = require('mongoose');
 const typeDefs = require('./graphql/typeDefs')
 const resolvers = require('./graphql/resolvers');
+const cors = require('cors');
 const { ApolloServer } = require('apollo-server-express');
 
 const SERVER_PORT = process.env.PORT || 3000
 const app = express();
 app.use(express.json()); // Make sure it comes back as json
+
+app.use(cors({
+  origin: ['http://localhost:4200'], 
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+}));
 
 //TODO - Replace you Connection String here
 mongoose.connect('mongodb+srv://Admin:pDiGDgich3CcFvBw@cluster0.2tyy8.mongodb.net/comp3133__101434395_assigment1?retryWrites=true&w=majority&appName=Cluster0', {
